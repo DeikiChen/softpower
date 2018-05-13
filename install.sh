bf5f2ceca31c9a27ac09c62c724988e29e1f6ef7
DeikiChen <deikichen@gmail.com>
2018-05-13 16:33:44 +0800

    V0.1

---
+++ install.sh
@@ -0,0 +1,44 @@
+
+#Step 1) Check if root--------------------------------------
+#if [[ $EUID -ne 0 ]]; then
+#   echo "Please execute script as root."
+#   exit 1
+#fi
+
+#Step 2) Download Python script-----------------------------
+cd /home/pi
+sudo mkdir SuperNESPi
+cd SuperNESPi
+
+script=softshutdown.py
+
+if [ -e $script ];
+	then
+		echo "Script softshutdown.py already exists. Doing nothing."
+	else
+		wget "https://raw.githubusercontent.com/DeikiChen/softpower/master/softshutdown.py"
+fi
+
+#
+
+#Step 4) Set sutostart ---------------------------
+cd ..
+cd .config
+sudo mkdir autostart
+cd autostart
+script=softshutdown.desktop
+
+if [ -e $script ];
+	then
+		echo "Script softshutdown.desktop already exists. Doing nothing."
+	else
+		wget "https://raw.githubusercontent.com/DeikiChen/softpower/master/softshutdown.desktop"
+fi
+#-------------------------------------------------------------
+#Step 7) Reboot to apply changes----------------------------
+echo "SuperNESPi Case installation done. Will now reboot after 3 seconds."
+sleep 3
+sudo reboot
+#-----------------------------------------------------------
+
+
