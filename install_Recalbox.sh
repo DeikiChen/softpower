64a805fb187d5d5dd5b59a509c9cdf121ee5767f
DeikiChen <deikichen@gmail.com>
2018-08-07 11:30:10 +0800

    Add files via upload
    fix

---
+++ install_Recalbox.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#Step 1 make /boot writable---------------------------------
+sleep 2s
+
+mount -o remount, rw /boot
+mount -o remount, rw /
+
+#--------------------------
+
+sleep 2s
+
+if grep -q "^system.power.switch=PIN356ONOFFRESET*" "/recalbox/share/system/recalbox.conf";
+	then
+		echo "PIN356ONOFFRESET configuration already enabled."
+	else
+		echo "system.power.switch=PIN356ONOFFRESET" >> /recalbox/share/system/recalbox.conf
+		echo "PIN356ONOFFRESET configuration enabled."
+fi
+#-----------------------------------------------------------
+
+#Step 3) Download Python script-----------------------------
+mkdir /opt/snespi
+sleep 2s
+
+script=/opt/snespi/SafeShutdown.py
+
+if [ -e $script ];
+	then
+		echo "Script SafeShutdown.py already exists. Doing nothing."
+	else
+		wget --no-check-certificate -O  $script "https://raw.githubusercontent.com/DeikiChen/softpower/master/recalbox_SafeShutdown.py"
+fi
+#-----------------------------------------------------------
+
+sleep 2s
+
+#Step 4) Enable Python script to run on start up------------
+DIR=/etc/init.d/S99SNESPi
+
+if grep -q "python $script &" "S99SNESPi";
+	then
+		if [ -x $DIR];
+			then
+				echo "Executable S99SNESPi already configured. Doing nothing."
+			else
+				chmod +x $DIR
+		fi
+	else
+		echo "python $script &" >> $DIR
+		chmod +x $DIR
+		echo "Executable S99SNESPi configured."
+fi
+#-----------------------------------------------------------
+
+#Step 5) Reboot to apply changes----------------------------
+echo "Super NESPi Case Switch installation done. Will now reboot after 3 seconds."
+sleep 3
+shutdown -r now
+#-----------------------------------------------------------
